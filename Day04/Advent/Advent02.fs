dba5981bef21d7a18e67ca655773c84f4256cd62
Oliver Lohmann <oliver.lohmann@microsoft.com>
2022-12-04 23:47:41 +0100

    Day 04. Cosmetics.

--- Day04/Advent/Advent02.fs
+++ Day04/Advent/Advent02.fs
@@ -1,22 +1,21 @@
 ﻿namespace Advent
-open System.IO
 
 module Part2 =
     let parseSequence(sequence: string) = 
         let i = sequence.Split("-")
         seq { i[0] |> int .. 1 .. i[1] |> int }
 
-    let parsePairs(line : string) =
+    let hasIntersection(line : string) =
         let i = line.Split(",") 
         seq { parseSequence(i[0]) |> Set.ofSeq; parseSequence(i[1]) |> Set.ofSeq }
         |> Set.intersectMany
-        |> Set.count > 0 
+        |> Set.count > 0
 
     let runInner(lines : string seq) =
         lines
-        |> Seq.map parsePairs
-        |> Seq.map (fun x -> if x then 1 else 0 )
-        |> Seq.sum
+        |> Seq.map hasIntersection
+        |> Seq.filter id // only take 'true's
+        |> Seq.length
 
     let run =
         Helper.readFile() 
